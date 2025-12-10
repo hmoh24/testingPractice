b7c8c2e6692665ed8155516c2111eb2f4a96c3b0
hmoh24 <hamsam0097@gmail.com>
2025-12-10 14:47:41 +0000

    add passing simple calculator with add/subtr/multp/div

---
+++ calculator.test.js
@@ -0,0 +1,23 @@
+const calculator = require("./calculator");
+
+describe("calculator", () => {
+  test("add", () => {
+    expect(calculator.add(5, 3)).toBe(8);
+  });
+
+  test("add", () => {
+    expect(calculator.add(1, 1203)).toBe(1204);
+  });
+
+  test("subtract", () => {
+    expect(calculator.subtract(10, 4)).toBe(6);
+  });
+
+  test("multiply", () => {
+    expect(calculator.multiply(5, 3)).toBe(15);
+  });
+
+  test("divide", () => {
+    expect(calculator.divide(9, 3)).toBe(3);
+  });
+});
